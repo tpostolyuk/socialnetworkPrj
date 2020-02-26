dc338468003ec19d50fce905a91067a13ca2dc25
Andrii <mrapostoliuk@gmail.com>
2020-02-26 11:37:48 +0200

    Changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,21 +7,6 @@ import * as firebase from 'firebase';
 
 
 class Header extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       nickName: 'Guest'
-    }
-  }
-  componentDidMount() {
-    let docRef = firebase.firestore().collection("nickname").doc("Name");
-    docRef.get().then(doc => {
-      doc.exists ? this.setState({nickName: doc.data().name}) : console.log('No such docs');
-    })
-    .catch(err => console.log(err.status, err.message));
-  }
-    
   render() {
    return (
       <header className={classes.header}>
@@ -34,7 +19,6 @@ class Header extends Component {
        <div className={classes.headerText}>
           <h1>my little blog</h1>
         </div>
-        <h1>{`Welcome ${ this.state.nickName } !`}</h1>
         <NavLink to="/auth"><Button 
             variant="contained"
             color="primary"
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,86 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import classes from './News.module.scss';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { addCard } from '../../redux/actions';
-import Card from '../News/Card';
-import { db } from '../../config';
 
-class News extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      title: '',
-      content: '',
-      dbData: {}
-    }
-  }
-  getData = () => {
-    const docRef = db.collection("posts").doc("post");
-    docRef.get()
-      .then(doc => doc.exists ? console.log('Data: ', doc.data()) : console.log('No such doc'))
-      .catch(error => console.log("Error getting document:", error))
-  
-  }
-  
-  handleSubmit = e => {
-    e.preventDefault();
-    db.collection("posts").doc("post").set({
-      title: this.state.title,
-      content: this.state.content
-    })
-      .then(() => console.log("Document successfully written!"))
-      .catch(error => console.error("Error writing document: ", error));
-    this.getData();
-  }
-  
-render() {
-  const { title, content } = this.state;
-  const { cards } = this.props;
-  const card = cards.map(i => <Card key={i} textTitle={title} textContent={content} />)
+const News = () => {
   return (
     <div className={classes.newsWrapper}>
-      <div className={classes.blockWrapper}>
-         <form onSubmit={this.handleSubmit}>
-          <TextField 
-            id="outlined-basic"
-            label="Title" 
-            variant="outlined"
-            onChange={e => this.setState({title: e.target.value})} />
-          <TextField 
-            id="outlined-basic" 
-            label="Content" 
-            variant="outlined"
-            onChange={e => this.setState({content: e.target.value})} />
-          <Button 
-            type="submit"
-            variant="contained" 
-            color="secondary"
-            onClick={() => this.props.addCard({ title, content })}>
-            Add Card
-          </Button>
-        </form> 
-      </div>
-      <div>
-        {card}
-      </div>
+
     </div>
     );
-  }
 }
 
-const mapStateToProps = state => {
-  return {
-    cards: state.card.cardList
-  }
-}
-const mapDispatchToProps = {
-  addCard
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps)
-  (News);
+export default News;
